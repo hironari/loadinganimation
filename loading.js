b28f186f77c69f78f265ddf77f02411f170ac3eb
hironari <hmmh3110@gmail.com>
2024-08-03 07:20:55 +0000

    全然動かなくなって来た

--- loading.js
+++ loading.js
@@ -8,18 +8,16 @@ window.onload = function() {
   // クラス 'spinner' の要素に対してクラスを追加
   const spinnerElements = document.getElementsByClassName('spinner');
   for (let i = 0; i < spinnerElements.length; i++) {
-    // アニメーションの終了を待ってからクラスを追加
-    setTimeout(() => {
+    spinnerElements[i].addEventListener('animationend', () => {
       spinnerElements[i].classList.add('loaded_spinner');
-    }, 1000); // アニメーションの時間に合わせて調整
+    });
   }
 
   // クラス 'logotype' の要素に対してクラスを追加
   const logotypeElements = document.getElementsByClassName('logotype');
   for (let i = 0; i < logotypeElements.length; i++) {
-    // アニメーションの終了を待ってからクラスを追加
-    setTimeout(() => {
+    logotypeElements[i].addEventListener('animationend', () => {
       logotypeElements[i].classList.add('loaded_spinner');
-    }, 1000); // アニメーションの時間に合わせて調整
+    });
   }
 }

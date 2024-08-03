9860d89629b94b39b99ada41c8d2b430481f56f9
hironari <hmmh3110@gmail.com>
2024-08-03 03:38:00 +0000

    animationがぶつかっていた

--- loading.js
+++ loading.js
@@ -5,15 +5,15 @@ window.onload = function() {
     loadingElement.classList.add('loaded');
   }
 
-  // クラス 'logotype' の要素に対してクラスを追加
-  const logotypeElements = document.getElementsByClassName('logotype');
-  for (let i = 0; i < logotypeElements.length; i++) {
-    logotypeElements[i].classList.add('loaded');
-  }
-
   // クラス 'spinner' の要素に対してクラスを追加
   const spinnerElements = document.getElementsByClassName('spinner');
   for (let i = 0; i < spinnerElements.length; i++) {
     spinnerElements[i].classList.add('loaded_spinner');
+  }
+
+  // クラス 'logotype' の要素に対してクラスを追加
+  const logotypeElements = document.getElementsByClassName('logotype');
+  for (let i = 0; i < logotypeElements.length; i++) {
+    logotypeElements[i].classList.add('loaded_spinner');
   }
 }

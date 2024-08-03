ee0792f961c60f2ac51f63b6ab83f47c4b98abc9
hironari <hmmh3110@gmail.com>
2024-08-03 04:39:47 +0000

    未解決

--- loading.js
+++ loading.js
@@ -8,12 +8,18 @@ window.onload = function() {
   // クラス 'spinner' の要素に対してクラスを追加
   const spinnerElements = document.getElementsByClassName('spinner');
   for (let i = 0; i < spinnerElements.length; i++) {
-    spinnerElements[i].classList.add('loaded_spinner');
+    // アニメーションの終了を待ってからクラスを追加
+    setTimeout(() => {
+      spinnerElements[i].classList.add('loaded_spinner');
+    }, 1000); // アニメーションの時間に合わせて調整
   }
 
   // クラス 'logotype' の要素に対してクラスを追加
   const logotypeElements = document.getElementsByClassName('logotype');
   for (let i = 0; i < logotypeElements.length; i++) {
-    logotypeElements[i].classList.add('loaded_spinner');
+    // アニメーションの終了を待ってからクラスを追加
+    setTimeout(() => {
+      logotypeElements[i].classList.add('loaded_spinner');
+    }, 1000); // アニメーションの時間に合わせて調整
   }
 }
